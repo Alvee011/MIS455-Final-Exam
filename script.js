b2bdd34a367e668de5caa73d1ac36b93d90bfcca
Alvee <alvee@example.com>
2026-08-22 21:23:23 +0600

    Update meal search script and styles

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var allArrayData = [];
+
 function connect() {
     var search = document.getElementById("userInput").value;
     var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
@@ -5,27 +7,45 @@ function connect() {
     fetch(url)
         .then(res => res.json())
         .then(data => display(data));
-
 }
 
 function display(data) {
-    var allArrayData = data.meals;
+    allArrayData = data.meals;
     var oldContent = document.getElementById("displayArea");
-    oldContent.textContent = "";
+    var showAllContent = document.getElementById("showAllArea");
 
-    for (var i = 1; i <= allArrayData.length; i++) {
+    // clear previous results
+    oldContent.textContent = "";
+    showAllContent.textContent = "";
+
+    if (allArrayData == null) {
+        oldContent.innerHTML = "<h3>No meals found!</h3>";
+        return;
+    }
+
+    // if more than 5 results, show only 5 first
+    var limit = allArrayData.length;
+    if (limit > 5) {
+        limit = 5;
+    }
+
+    for (var i = 0; i < limit; i++) {
         var newDiv = document.createElement("div");
-        newDiv.innerHTML = `Meal Title: <b> ${allArrayData[i - 1].strMeal} </b><br>
-                           <img src="${allArrayData[i - 1].strMealThumb}">  <br> 
-                           Meal Country: ${allArrayData[i - 1].strCountry}  <br>
-                           Meal Category: ${allArrayData[i - 1].strCategory}`;
+        newDiv.innerHTML = `Meal ID: <b>${allArrayData[i].idMeal}</b> <br>
+                            Meal Name: <b>${allArrayData[i].strMeal}</b> <br>
+                            <img src="${allArrayData[i].strMealThumb}"> <br>
+                            Meal Title: ${allArrayData[i].strCategory} <br>
+                            Cooking Instructions: <p>${allArrayData[i].strInstructions}</p>`;
 
         newDiv.classList.add("innerStyle");
         oldContent.appendChild(newDiv);
-
     }
 
-
-
-
+    // Add SHOW ALL button if more than 5 results exist
+    if (allArrayData.length > 5) {
+        var btn = document.createElement("button");
+        btn.textContent = "SHOW ALL";
+        btn.onclick = showAll;
+        showAllContent.appendChild(btn);
+    }
 }

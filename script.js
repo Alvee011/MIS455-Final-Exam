bd01632d9045dfa06924cc09a813545e0e632d7d
Alvee <alvee@example.com>
2026-08-22 21:01:25 +0600

    Update meal search and display results

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-function start() {
+function connect() {
     var search = document.getElementById("userInput").value;
-    var api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
 
-    fetch(api)
+    fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => display(data));
+
+}
+
+function display(data) {
+    var allArrayData = data.meals;
+    var oldContent = document.getElementById("displayArea");
+    oldContent.textContent = "";
+
+    for (var i = 1; i <= allArrayData.length; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `Meal Title: <b> ${allArrayData[i - 1].strMeal} </b><br>
+                           <img src="${allArrayData[i - 1].strMealThumb}">  <br> 
+                           Meal Country: ${allArrayData[i - 1].strCountry}  <br>
+                           Meal Category: ${allArrayData[i - 1].strCategory}`;
+
+        newDiv.classList.add("innerStyle");
+        oldContent.appendChild(newDiv);
+
+    }
+
+
+
 
 }
